bb28b51849fac339b21eeb1417611d8d5ebef1f6
kishu3455-arch <kishu3455@icloud.com>
2026-05-07 19:19:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,17 @@ from supabase import create_client
 
 app = Flask(__name__)
 
-url = "https://himwwdufpswsblssnvrw.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpbXd3ZHVmcHN3c2Jsc3NudnJ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg5MzU3MzYsImV4cCI6MjA2NDUxMTczNn0.rKm40TepoHwU7i3k62OHIJI5mMZL2p4SXTnXY14aNgc"
+url = "https://gairuvgaqonaequ....supabase.co"  # SUPABASE NUNCHI FULL URL PASTE CHEY
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."  # SUPABASE NUNCHI ANON KEY PASTE CHEY
 supabase = create_client(url, key)
 
 @app.route('/buses')
 def get_buses():
     try:
         data = supabase.table("buses").select("*").execute()
-        return jsonify({"success": True, "count": len(data.data), "buses": data.data})
+        return jsonify({"success": True, "count": len(data.data), "data": data.data})
     except Exception as e:
-        return jsonify({"success": False, "error": str(e)}), 500
+        return jsonify({"success": False, "error": str(e)})
 
 if __name__ == '__main__':
     app.run()
-    
